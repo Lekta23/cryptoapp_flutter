5e7ef294ad57dd827d9d6a4fb5edaab558b2abe2
FORLASV <93997804+FORLASV@users.noreply.github.com>
2021-11-10 11:16:08 +0100

    Import Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notreprojet/navabar.dart';
 
-import 'globals.dart';
+// import 'globals.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,58 +28,5 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Globals.secondaryColor,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-    items: const <BottomNavigationBarItem>[
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        label: 'Accueil',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.settings),
-        label: 'Réglages',
-      ),
-    ],
-  ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-        backgroundColor: Globals.secondaryColor,
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  State<MyHomePage> createState() => Navbar();
 }
---
+++ lib/navabar.dart
@@ -0,0 +1,68 @@
+
+import 'globals.dart';
+import 'package:flutter/material.dart';
+import 'package:notreprojet/main.dart';
+
+class Navbar extends State<MyHomePage> {
+  // int _counter = 0;
+  int _selectedIndex = 0;
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+  void _onItemTapped(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Globals.secondaryColor,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+    items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.wallet_travel),
+        label: 'Wallet',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.change_circle),
+        label: 'Change',
+      ),
+    ],
+    currentIndex: _selectedIndex,
+    onTap: _onItemTapped, 
+  ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'You have pushed the button this many times:',
+      //       ),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headline4,
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      //   backgroundColor: Globals.secondaryColor,
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
