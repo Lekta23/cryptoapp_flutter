7d17d11da2ab020cb701fa86243bb1b800ccdcd5
Clément LEFIEF <clement.lefief@gmail.com>
2021-11-10 10:57:47 +0100

    add globals

---
+++ lib/globals.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Globals {
+  static const Color primaryColor = Color(0xff12202f);
+  static const Color secondaryColor = Color(0xff1c2834);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'globals.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,18 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -74,6 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Globals.secondaryColor,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -109,6 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+        backgroundColor: Globals.secondaryColor,
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
