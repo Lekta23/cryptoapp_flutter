489badfb70d478dbd0f6c1038f2c7cb58c3838d8
FORLASV <93997804+FORLASV@users.noreply.github.com>
2021-11-17 12:49:59 +0100

    profile and settings

--- lib/screens/home/localwidget/appbar.dart
+++ lib/screens/home/localwidget/appbar.dart
@@ -1,3 +1,6 @@
+import 'package:notreprojet/screens/profile/profile_page.dart';
+import 'package:notreprojet/screens/settings/settings_page.dart';
+
 import '../../../globals.dart';
 import 'package:flutter/material.dart';
 
@@ -9,10 +12,43 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-return AppBar(
-  title: titre,
-  backgroundColor: backgroundColor,
-);
+    return AppBar(
+      title: titre,
+      backgroundColor: backgroundColor,
+      actions: [
+        PopupMenuButton<int>(
+          onSelected: (item) => onSelected(context,item),
+          itemBuilder: (context) => [
+            const PopupMenuItem<int>(
+              value: 0,
+              child: Text('Settings'),
+            ),
+            const PopupMenuItem<int>(
+              value: 1,
+              child: Text('Profile'),
+            ),
+          ],
+        ),
+      ],
+      
+    );
+    
+  }
+
+void onSelected(BuildContext context, int item){
+    switch(item){
+      case 0:
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => SettingsPage())
+        );
+        break;
+      case 1:
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => ProfilePage())
+        );
+        break;
+      
+    }  
 }
 
   @override
