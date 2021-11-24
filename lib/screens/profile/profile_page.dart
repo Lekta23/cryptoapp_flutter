96a16d8c91606f793395773cade4281dce2359b2
FORLASV <93997804+FORLASV@users.noreply.github.com>
2021-11-24 11:04:12 +0100

    profile page

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -1,13 +1,282 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import '../../globals.dart';
+
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: Text("Profile".toUpperCase()),
-      centerTitle: true,
-      backgroundColor: Colors.pink,
-    ),
-  );
+      appBar: AppBar(
+        title: Text("Profile".toUpperCase()),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+            Card(
+              color: Globals.secondaryColor,
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: double.infinity,
+                        child: Container(
+                          alignment: Alignment(0.0, 2.5),
+                          child: const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "Add you profile DP image URL here "),
+                            radius: 60.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Adrien",
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            "Poulain",
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          RaisedButton(
+                            onPressed: () {},
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(80.0),
+                            ),
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                gradient: const LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [Colors.pink, Colors.redAccent]),
+                                borderRadius: BorderRadius.circular(80.0),
+                              ),
+                              child: Container(
+                                constraints: const BoxConstraints(
+                                  maxWidth: 100.0,
+                                  maxHeight: 40.0,
+                                ),
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  "Portfolio",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12.0,
+                                      letterSpacing: 2.0,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Globals.secondaryColor,
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: double.infinity,
+                        child: Container(
+                          alignment: Alignment(0.0, 2.5),
+                          child: const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "Add you profile DP image URL here "),
+                            radius: 60.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Clément",
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            "Lefief",
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          RaisedButton(
+                            onPressed: () {},
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(80.0),
+                            ),
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                gradient: const LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [Colors.pink, Colors.redAccent]),
+                                borderRadius: BorderRadius.circular(80.0),
+                              ),
+                              child: Container(
+                                constraints: const BoxConstraints(
+                                  maxWidth: 100.0,
+                                  maxHeight: 40.0,
+                                ),
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  "Portfolio",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12.0,
+                                      letterSpacing: 2.0,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Globals.secondaryColor,
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: double.infinity,
+                        child: Container(
+                          alignment: Alignment(0.0, 2.5),
+                          child: const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "Add you profile DP image URL here "),
+                            radius: 60.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Mathieu",
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            "Boidin",
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          RaisedButton(
+                            onPressed: () {},
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(80.0),
+                            ),
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                gradient: const LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [Colors.pink, Colors.redAccent]),
+                                borderRadius: BorderRadius.circular(80.0),
+                              ),
+                              child: Container(
+                                constraints: const BoxConstraints(
+                                  maxWidth: 100.0,
+                                  maxHeight: 40.0,
+                                ),
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  "Portfolio",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12.0,
+                                      letterSpacing: 2.0,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            )
+          ],
+        ),
+      ));
 }
