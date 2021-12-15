f8c617dd3e9f240d9e083627eca517f0956367ca
Clément LEFIEF <clement.lefief@gmail.com>
2021-12-15 17:44:53 +0100

    coucou

--- lib/screens/home/localwidget/cryptocard.dart
+++ lib/screens/home/localwidget/cryptocard.dart
@@ -21,12 +21,11 @@ class CryptoCard extends ConsumerWidget {
     required this.data,
   }) : super(key: key);
 
-
   final List<Widget> _painters = <Widget>[];
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-   MaterialColor favOFF = Colors.grey;
-  MaterialColor favON = Colors.yellow;
+    MaterialColor favOFF = Colors.grey;
+    MaterialColor favON = Colors.yellow;
     var priceInt = double.parse(data.price);
     var priceString = priceInt.toStringAsFixed(2);
     var oneYearDouble = double.parse(data.oneYear!.price_change_pct.toString());
@@ -65,119 +64,132 @@ class CryptoCard extends ConsumerWidget {
     }
 
     return GestureDetector(
-      onTap: () {
-        _onTap(data, context);
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SizedBox(
-          width: 190,
-          height: 260,
-          child: Card(
-            color: Globals.secondaryColor,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      afficheImage(data.logo_url),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(data.name,
+        onTap: () {
+          _onTap(data, context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            width: 190,
+            height: 260,
+            child: Card(
+              color: Globals.secondaryColor,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        afficheImage(data.logo_url),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(data.name,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                color: Globals.text1,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700,
+                              )),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                    '1D: ' +
+                                        data.oneDay!.price_change_pct
+                                            .toString() +
+                                        '% ' +
+                                        AfficheFleche(data
+                                            .oneDay!.price_change_pct
+                                            .toString()),
+                                    style: const TextStyle(
+                                      color: Globals.text1,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily: 'RobotoMono',
+                                    )),
+                                const SizedBox(height: 4),
+                                Text(
+                                    '1M: ' +
+                                        data.oneMonth!.price_change_pct
+                                            .toString() +
+                                        '% ' +
+                                        AfficheFleche(data
+                                            .oneMonth!.price_change_pct
+                                            .toString()),
+                                    style: const TextStyle(
+                                      color: Globals.text1,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily: 'RobotoMono',
+                                    )),
+                                const SizedBox(height: 4),
+                                Text(
+                                    '1Y: ' +
+                                        oneYearString +
+                                        '% ' +
+                                        AfficheFleche(data
+                                            .oneYear!.price_change_pct
+                                            .toString()),
+                                    style: const TextStyle(
+                                      color: Globals.text1,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily: 'RobotoMono',
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Text(priceString + ' USD',
                             style: const TextStyle(
                               color: Globals.text1,
                               fontSize: 18,
-                              fontWeight: FontWeight.w700,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'RobotoMono',
                             )),
-                      ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text('1D: ' + data.oneDay!.price_change_pct.toString() + '% ' + AfficheFleche(data.oneDay!.price_change_pct.toString()),
-                                style: const TextStyle(
-                                  color: Globals.text1,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600,
-                                  fontFamily: 'RobotoMono',
-                                )),
-                            const SizedBox(height: 4),
-                            Text(
-                                '1M: ' +
-                                    data.oneMonth!.price_change_pct.toString() +
-                                    '% ' +
-                                    AfficheFleche(data.oneMonth!.price_change_pct.toString()),
-                                style: const TextStyle(
-                                  color: Globals.text1,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600,
-                                  fontFamily: 'RobotoMono',
-                                )),
-                            const SizedBox(height: 4),
-                            Text(
-                                '1Y: ' +
-                                    oneYearString +
-                                    '% ' +
-                                    AfficheFleche(data.oneYear!.price_change_pct.toString()),
-                                style: const TextStyle(
-                                  color: Globals.text1,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600,
-                                  fontFamily: 'RobotoMono',
-                                )),
-                          ],
-                        ),
-                      ),
+                      ],
                     ),
-                    Text(priceString + ' USD',
-                        style: const TextStyle(
-                          color: Globals.text1,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          fontFamily: 'RobotoMono',
-                        )),
-                  ],
-                ),
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: ref.watch(favoritesProvider).map(
-                    data: (data) {
-                      return IconButton(
-                        icon: Icon(
-                          Icons.favorite,
-                          color: data.value.contains(name) ? favON : favOFF,
-                        ),
-                        tooltip: 'Add to favourite',
-                        onPressed: () async {
-                          final preferences =
-                              await StreamingSharedPreferences.instance;
-                          Preference<List<String>> preflist = preferences
-                              .getStringList('listFav', defaultValue: []);
-                          final List<String> list = preflist.getValue();
-                          if (list.indexOf(name) < 0) {
-                            list.add(name);
-                          } else {
-                            list.remove(name);
-                          }
-                          preferences.setStringList('listFav', list);
-                          ref.refresh(favoritesProvider);
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: ref.watch(favoritesProvider).map(
+                        data: (data) {
+                          return IconButton(
+                            icon: Icon(
+                              Icons.favorite,
+                              color: data.value.contains(name) ? favON : favOFF,
+                            ),
+                            tooltip: 'Add to favourite',
+                            onPressed: () async {
+                              final preferences =
+                                  await StreamingSharedPreferences.instance;
+                              Preference<List<String>> preflist = preferences
+                                  .getStringList('listFav', defaultValue: []);
+                              final List<String> list = preflist.getValue();
+                              if (list.indexOf(name) < 0) {
+                                list.add(name);
+                              } else {
+                                list.remove(name);
+                              }
+                              preferences.setStringList('listFav', list);
+                              ref.refresh(favoritesProvider);
+                            },
+                          );
                         },
-                      );
-                    },
-                    error: (error) => const SizedBox(),
-                    loading: (loading) => const SizedBox()),
+                        error: (error) => const SizedBox(),
+                        loading: (loading) => const SizedBox()),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 
   void _onTap(Currency currency, context) {
