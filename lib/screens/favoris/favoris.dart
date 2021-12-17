c476d398ca4a2041d23aedb6c4a26444e35ef9f3
FORLASV <93997804+FORLASV@users.noreply.github.com>
2021-12-17 09:52:57 +0100

    Update favoris.dart

--- lib/screens/favoris/favoris.dart
+++ lib/screens/favoris/favoris.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:notreprojet/globals.dart';
 import 'package:notreprojet/providers/prefs.provider.dart';
-import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 
 class Favoris extends ConsumerWidget {
   const Favoris({Key? key}) : super(key: key);
